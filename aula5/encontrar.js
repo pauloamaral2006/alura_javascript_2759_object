755181de85c5bd5f058bfa69ab0bc234a529e12b
Paulo Amaral <pauloamaral2006@yahoo.com.br>
2026-03-11 22:22:13 -0300

    Aula 05

---
+++ aula5/encontrar.js
@@ -0,0 +1,13 @@
+const clientes = require("./clientes.json");
+
+function encontrar(lista, chave, valor) {
+  return lista.find((item) => item[chave].includes(valor));
+}
+
+const encontrado = encontrar(clientes, "nome", "Kirby");
+
+console.log(encontrado);
+
+const encontrado2 = encontrar(clientes, "telefone", "1918820860");
+
+console.log(encontrado2);
