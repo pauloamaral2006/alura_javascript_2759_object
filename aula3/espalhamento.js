6594496b2b00468bf1fb4dd99992468d6272c4f1
Paulo Amaral <pauloamaral2006@yahoo.com.br>
2026-03-11 21:57:15 -0300

    Aula 03

---
+++ aula3/espalhamento.js
@@ -0,0 +1,22 @@
+const cliente = {
+  nome: "Joao",
+  idade: 24,
+  email: "joao@firma.com",
+  telefone: ["1155555550", "1144444440"],
+};
+
+cliente.enderecos = [
+  {
+    rua: "R. Joseph Climber",
+    numero: 1337,
+    apartamento: true,
+    complemento: "ap 934",
+  },
+];
+
+const encomenda = {
+  destinatario: cliente.nome,
+  ...cliente.enderecos[0],
+};
+
+console.log(encomenda);
